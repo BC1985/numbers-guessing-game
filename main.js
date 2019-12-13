ea19c2572238f0a92dd9479ca776e659db14d53c
Ben Cohen <benjcohen1985@gmail.com>
2019-12-13 08:43:38 -0500

    Move end game message to top panel
    Display number of attemtps on game over
    
    Disable buttons once game started
    
    Change number of remaining attempts on difficulty setting

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const userGuessLog = [];
 let remainingAttempts = 10;
 let maxGuesses = 10;
+numberOfAttempts = 0;
 let numberToGuess = Math.floor(Math.random() * 100 + 1);
 
+const container = document.getElementById("container");
+const attemptsOutput = document.getElementById("attempts");
+const userInterface = document.getElementById("user-interface");
+const attemptsCounter = document.getElementById("attempts-counter");
 easyMode = () => {
   maxGuesses = 10;
-  document.getElementById("easy-button").classList.toggle("easy-button");
+  remainingAttempts = 10;
+  attemptsOutput.innerHTML = remainingAttempts;
+  document.getElementById("easy-button").classList.add("easy-button");
   document.getElementById("hard-button").classList.remove("hard-button");
 };
 
 hardMode = () => {
   maxGuesses = 5;
-  document.getElementById("hard-button").classList.toggle("hard-button");
+  remainingAttempts = 5;
+  attemptsOutput.innerHTML = remainingAttempts;
+  document.getElementById("hard-button").classList.add("hard-button");
   document.getElementById("easy-button").classList.remove("easy-button");
 };
 
@@ -20,8 +29,6 @@ newGame = () => {
 };
 endGame = () => {
   document.getElementById("new-game-button").style.display = "inline";
-  document.getElementById("easy-button").style.display = "none";
-  document.getElementById("hard-button").style.display = "none";
   document.getElementById("input-box").setAttribute("readOnly", true);
 };
 
@@ -29,12 +36,17 @@ clearInput = () => {
   document.getElementById("input-box").value = "";
 };
 compareGuess = () => {
+  numberOfAttempts++;
+  const userWonMessage = `<h1>that is correct, oh wise one!</h1> <p>you guessed it in ${numberOfAttempts} ${determineIfSingularNoun()}. let us make merry and rejoice and may the halls of valhala forever echo thy name!</p>`;
+  const userLostMessage = `<h1>You lose, puny mortal!</h1> <p>The correct number was ${numberToGuess}</p>`;
+  // disable buttons when once input entered and game started
+  document.getElementById("hard-button").disabled = true;
+  document.getElementById("easy-button").disabled = true;
   let userGuess = document.getElementById("input-box").value;
-  let attemptsOutput = document.getElementById("attempts");
   let textOutput = document.getElementById("text-output");
   let guessLogOutput = document.getElementById("guess-log");
 
-  userGuessLog.push(userGuess);
+  userGuessLog.push(`${userGuess} `);
   guessLogOutput.innerHTML = userGuessLog;
   remainingAttempts--;
   attemptsOutput.innerHTML = remainingAttempts;
@@ -47,18 +59,39 @@ compareGuess = () => {
       textOutput.innerHTML = "Try a lower number.";
       clearInput();
     } else {
-      textOutput.innerHTML = `That is correct, oh wise one! You guessed it in ${attempts} attempts. Let us make merry and rejoice!`;
+      userInterface.innerHTML = userWonMessage;
+      attemptsCounter.remove();
+      container.className += " user-won";
+
+      clearInput();
       endGame();
     }
   } else {
-    // on the last guessing attempt
+    // on the last guessing attempt if number is wrong
     if (userGuess > numberToGuess || userGuess < numberToGuess) {
-      textOutput.innerHTML = `You lose, puny mortal! The correct number was ${numberToGuess}`;
+      numberOfAttempts++;
+      // display message
+      userInterface.innerHTML = userLostMessage;
+      // change style upon losing
+      container.className += " user-lost";
       clearInput();
       endGame();
     } else {
-      textOutput.innerHTML = `That is correct, oh wise one! You guessed it in ${attempts} attempts. Let us make merry and rejoice!`;
+      // Or if user is right
+      container.className += " user-won";
+
+      userInterface.innerHTML = userWonMessage;
+      attemptsCounter.remove();
+      clearInput();
       endGame();
     }
+  }
+};
+// match feedback to number of guesses made
+determineIfSingularNoun = () => {
+  if (numberOfAttempts === 1) {
+    return "attempt";
+  } else {
+    return "attempts";
   }
 };
